46094d2ff3dffd35cb364ff08657e009b423dcf8
yurakas97 <123570342+yurakas97@users.noreply.github.com>
2024-03-22 10:14:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let currentTokenID;
 
 async function executeCommand() {
   const command = "node scripts/private/_send-nft-info-config.js";
-  await fetch('http://213.111.123.79:3000/execute-command', {
+  await fetch('https://evil-maps-eat.loca.lt/execute-command', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
